e171a4104e5e253ae4a1408084185be1844a0361
京 <41622233+kuangbj@users.noreply.github.com>
2018-12-07 16:26:31 +0800

    Update init.php

--- src/assets/data/init.php
+++ src/assets/data/init.php
@@ -3,6 +3,7 @@
 *此页面不应被客户端直接请求，需要被其它PHP页面包含
 **/
 header('Content-Type: application/json;charset=UTF-8');
+header("Access-Control-Allow-Origin: *"); 
 
 $conn = mysqli_connect('127.0.0.1','root','','mfresh',3306);
 $sql = "SET NAMES UTF8";
